933a7109888ac1bf726202cddc24fa4effe47774
StackDoubleFlow <ojaslandge@gmail.com>
2022-06-13 04:33:47 -0500

    Add ndk include directory for bindgen

--- applier/build.rs
+++ applier/build.rs
@@ -1,6 +1,15 @@
 use std::env::{self, VarError};
 use std::path::PathBuf;
 
+#[cfg(target_os = "linux")]
+const NDK_HOST_TAG: &str = "linux-x86_64";
+#[cfg(target_os = "macos")]
+const NDK_HOST_TAG: &str = "darwin-x86_64";
+#[cfg(all(target_os = "windows", target_arch = "x86"))]
+const NDK_HOST_TAG: &str = "windows";
+#[cfg(all(target_os = "windows", target_arch = "x86_64"))]
+const NDK_HOST_TAG: &str = "windows-x86_64";
+
 fn main() {
     // TODO: Find a matching unity install for every enabled il2cpp version and generate bindings for each
     let editors_path = match env::var("UNITY_EDITORS") {
@@ -11,9 +20,22 @@ fn main() {
         Err(e) => panic!("{:?}", e),
     };
 
+    let ndk_path = match env::var("ANDROID_NDK_HOME") {
+        Ok(path) => PathBuf::from(path),
+        Err(VarError::NotPresent) => {
+            panic!("please set $ANDROID_NDK_HOME to the path of your ndk installation")
+        }
+        Err(e) => panic!("{:?}", e),
+    };
+    let ndk_sysroot_include = ndk_path.join(format!(
+        "toolchains/llvm/prebuilt/{}/sysroot/usr/include",
+        NDK_HOST_TAG
+    ));
+
     let libil2cpp_path = editors_path.join("2019.4.28f1/Editor/Data/il2cpp/libil2cpp");
     let bindings = bindgen::Builder::default()
         .clang_arg(format!("-I{}", libil2cpp_path.to_str().unwrap()))
+        .clang_arg(format!("-isystem{}", ndk_sysroot_include.to_str().unwrap()))
         .header(
             libil2cpp_path
                 .join("il2cpp-class-internals.h")
