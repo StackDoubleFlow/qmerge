1ae1c2ea2b58c77ac5be08d9cb04ec1f732b2523
StackDoubleFlow <ojaslandge@gmail.com>
2022-06-18 01:50:49 -0500

    Copy generic method bodies

--- tools/DllsGen/DllsGen/Program.cs
+++ tools/DllsGen/DllsGen/Program.cs
@@ -1,11 +1,17 @@
 ﻿using AsmResolver.DotNet;
+using AsmResolver.DotNet.Code.Cil;
+using AsmResolver.DotNet.Collections;
 using AsmResolver.DotNet.Serialized;
+using AsmResolver.DotNet.Signatures;
+using AsmResolver.DotNet.Signatures.Types;
+using AsmResolver.PE.DotNet.Cil;
 using AsmResolver.PE.DotNet.Metadata.Tables.Rows;
 
 // TODO: cli interface thing
 var publicizeMethods = false;
 var managedPath = "/home/stack/ledump/QuestModding/qmerge/example_mod/build/Managed/";
 var inputPath = "/home/stack/ledump/QuestModding/qmerge/test_analysis/cpp2il_out";
+var referencePath = "/home/stack/.local/share/Steam/steamapps/common/Beat Saber/Beat Saber_Data/Managed/";
 
 if (Directory.Exists(managedPath))
 {
@@ -13,30 +19,116 @@ if (Directory.Exists(managedPath))
     Directory.CreateDirectory(managedPath);
 }
 
-var dummyPaths = Directory.GetFiles(inputPath);
+var inputPaths = Directory.GetFiles(inputPath);
 
 var readingParams = new ModuleReaderParameters(inputPath);
 
 var dummyModule = new ModuleReference("MergePInvokeDummy");
 
-static void ProcessType(TypeDefinition type, ModuleReference dummyModule, bool publicizeMethods)
+static void ProcessType(TypeDefinition type, ModuleDefinition module, ModuleReference dummyModule, ModuleDefinition referenceModule)
 {
-    foreach (var method in type.Methods.Where(method => method.IsPInvokeImpl))
+    var referenceTypeRef = referenceModule.CreateTypeReference(type.Namespace, type.Name!);
+    var referenceType = referenceModule.MetadataResolver.ResolveType(referenceTypeRef);
+
+    foreach (var method in type.Methods)
     {
-        const ImplementationMapAttributes attributes = ImplementationMapAttributes.NoMangle | ImplementationMapAttributes.CharSetAuto | ImplementationMapAttributes.CallConvCdecl;
-        method.ImplementationMap = new ImplementationMap(dummyModule, "MergePInvokeDummy", attributes);
+        if (method.IsPInvokeImpl)
+        {
+            const ImplementationMapAttributes attributes = ImplementationMapAttributes.NoMangle | ImplementationMapAttributes.CharSetAuto | ImplementationMapAttributes.CallConvCdecl;
+            method.ImplementationMap = new ImplementationMap(dummyModule, "MergePInvokeDummy", attributes);
+        }
+
+        if (type.GenericParameters.Count > 0 || method.GenericParameters.Count > 0)
+        {
+            var referenceMethod = (MethodDefinition?) referenceType?.CreateMemberReference(method.Name, method.Signature).Resolve();
+            if (referenceMethod?.CilMethodBody == null) continue;
+        
+            var body = new CilMethodBody(method);
+            var referenceBody = referenceMethod.CilMethodBody;
+                
+            try
+            {
+                foreach (var instruction in referenceBody.Instructions)
+                {
+                    var newOperand = instruction.Operand;
+                    switch (instruction.Operand)
+                    {
+                        case MemberReference reference:
+                        {
+                            if (reference.IsField)
+                            {
+                                newOperand = module.DefaultImporter.ImportField(((IFieldDescriptor) reference).Resolve());
+                            } 
+                            else if (reference.IsMethod)
+                            {
+                                newOperand = module.DefaultImporter.ImportMethod(((IMethodDescriptor) reference).Resolve());
+                            }
+                            break;
+                        }
+                        case IMethodDescriptor descriptor:
+                        {
+                            newOperand = module.DefaultImporter.ImportMethod(descriptor);
+                            break;
+                        }
+                        case IFieldDescriptor descriptor:
+                        {
+                            newOperand = module.DefaultImporter.ImportField(descriptor);
+                            break;
+                        }
+                        case TypeSpecification spec:
+                        {
+                            newOperand = spec.ImportWith(module.DefaultImporter);
+                            break;
+                        }
+                        case ITypeDescriptor def:
+                        {
+                            newOperand = module.DefaultImporter.ImportType(def.Resolve());
+                            break;
+                        }
+                        case CilInstructionLabel label:
+                            newOperand = new CilOffsetLabel(label.Offset);
+                            break;
+                        case List<ICilLabel> list:
+                            newOperand = list.Select(label => (ICilLabel) new CilOffsetLabel(label.Offset)).ToList();
+                            break;
+                        case null:
+                        case sbyte:
+                        case int:
+                        case long: 
+                        case float:
+                        case double:
+                        case string:
+                        case Parameter:
+                        case CilLocalVariable:
+                            break;
+                        default:
+                            Console.WriteLine($"Unhandled operand type while copying method {method}: " + newOperand.GetType().FullName);
+                            break;
+                    }
+                    body.Instructions.Add(new CilInstruction(instruction.OpCode, newOperand));
+                }
+                method.CilMethodBody = body;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error copying method: {method}");
+                Console.WriteLine(e);
+            }
+        }
     }
 }
 
-foreach (var path in dummyPaths)
+foreach (var path in inputPaths)
 {
     var fileName = Path.GetFileName(path);
     var module = ModuleDefinition.FromFile(path, readingParams);
 
-    var imported = module.DefaultImporter.ImportModule(dummyModule);
+    var referenceModule = ModuleDefinition.FromFile(referencePath + fileName);
+
+    var importedDummy = module.DefaultImporter.ImportModule(dummyModule);
     foreach (var type in module.GetAllTypes())
     {
-        ProcessType(type, imported, publicizeMethods);
+        ProcessType(type, module, importedDummy, referenceModule);
     }
     
     module.Write(managedPath + fileName);
