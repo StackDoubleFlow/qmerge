e27ba1651796c7e00592a09be50ca49ce81725dc
StackDoubleFlow <ojaslandge@gmail.com>
2022-07-08 20:06:51 -0500

    Clean up example

--- MergeExample/src/Example.cs
+++ MergeExample/src/Example.cs
@@ -1,25 +1,12 @@
 using UnityEngine;
-using System;
 using System.Reflection;
-using MergeExample;
-using MergeExample.Hooks;
 using QMerge.Hooking;
-using TMPro;
-using UnityEngine.SceneManagement;
 
 public class Plugin
 {
     public static void Init()
     {
         Debug.Log("Initializing MergeExample");
-        Attribute[] attrs = Attribute.GetCustomAttributes(typeof(MainMenuViewControllerDidActivate), false);  // Reflection.  
-  
-        // Displaying output.  
-        foreach (Attribute attr in attrs)  
-        {
-            Debug.Log(attr.GetType().FullName);
-        }
-
         var hookManager = new HookManager();
         hookManager.HookAll(Assembly.GetExecutingAssembly());
     }
--- MergeExample/src/Hooks/MainMenuViewControllerDidActivate.cs
+++ MergeExample/src/Hooks/MainMenuViewControllerDidActivate.cs
@@ -1,18 +1,10 @@
 using QMerge.Hooking;
-using UnityEngine;
 
 namespace MergeExample.Hooks
 {
     [Hook(typeof(MainMenuViewController), "DidActivate")]
     public class MainMenuViewControllerDidActivate
     {
-        public static void Postfix(MusicPackPromoBanner ____musicPackPromoBanner)
-        {
-            Debug.Log("in postfix");
-            Debug.Log(____musicPackPromoBanner);
-            ____musicPackPromoBanner.gameObject.SetActive(false);
-            Debug.Log("done with postfix");
-        } 
-            
+        public static void Postfix(MusicPackPromoBanner ____musicPackPromoBanner) => ____musicPackPromoBanner.gameObject.SetActive(false);
     }
 }
